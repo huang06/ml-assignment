0fc35001a3604c8d82a430254cd6f73da553e5b9
huang06 <tom4996131207@gmail.com>
2024-02-19 17:44:30 +0000

    feat: update

--- app/main.py
+++ app/main.py
@@ -1,10 +1,45 @@
 from __future__ import annotations
 
+import os
+
 from fastapi import FastAPI
+from pydantic import BaseModel
+from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
+
+
+class Record(BaseModel):
+    id: str
+    text: str
+
+
+class TranslationPayload(BaseModel):
+    fromLang: str
+    records: list[Record]
+    toLang: str
+
+
+class RequestModel(BaseModel):
+    payload: TranslationPayload
+
+
+LLM_DIR = os.environ["LLM_DIR"]
+
+model = M2M100ForConditionalGeneration.from_pretrained(LLM_DIR)
+tokenizer = M2M100Tokenizer.from_pretrained(LLM_DIR, src_lang="en", tgt_lang="fr")
 
 app = FastAPI()
 
 
 @app.post("/translation")
-def translation(text: str, source_language: str, target_language: str) -> dict:
-    return {"result": "人生はチョコレートの箱のようなものだ。"}
+async def translation(request_model: RequestModel) -> dict:
+    print(request_model)
+    payload = request_model.payload
+    tokenizer.src_lang = payload.fromLang
+    result = []
+    for record in payload.records:
+        model_inputs = tokenizer(record.text, return_tensors="pt")
+        generated_tokens = model.generate(**model_inputs, forced_bos_token_id=tokenizer.get_lang_id(payload.toLang))
+        resp_list = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+        result.append({"id": record.id, "text": resp_list[0]})
+    print(result)
+    return {"result": result}
--- app/translation_example.py
+++ app/translation_example.py
@@ -1,9 +1,18 @@
 from __future__ import annotations
 
-from transformers import M2M100Config, M2M100ForConditionalGeneration, M2M100Tokenizer
+import os
+from pathlib import Path
 
-model = M2M100ForConditionalGeneration.from_pretrained('facebook/m2m100_418M')
-tokenizer = M2M100Tokenizer.from_pretrained('facebook/m2m100_418M', src_lang="en", tgt_lang="fr")
+from transformers import M2M100ForConditionalGeneration, M2M100Tokenizer
+
+here = Path(__file__).resolve().parent
+LLM_DIR = os.environ.get("LLM_DIR") or str(
+    here / "../artifacts/models--facebook--m2m100_418M/snapshots/62d980b8566a7c30e96918baf450d6a7218aadec"
+)
+model = M2M100ForConditionalGeneration.from_pretrained(LLM_DIR)  # ("facebook/m2m100_418M")
+tokenizer = M2M100Tokenizer.from_pretrained(
+    LLM_DIR, src_lang="en", tgt_lang="fr"
+)  # ("facebook/m2m100_418M", src_lang="en", tgt_lang="fr")
 
 
 if __name__ == "__main__":
@@ -12,8 +21,10 @@ if __name__ == "__main__":
     tgt_lang = "人生はチョコレートの箱のようなものだ。"
 
     tokenizer.src_lang = "en"
+    tokenizer.tgt_lang = "ja"
     model_inputs = tokenizer(src_text, return_tensors="pt")
     generated_tokens = model.generate(**model_inputs, forced_bos_token_id=tokenizer.get_lang_id("ja"))
     result = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
 
     print(result[0])
+    print(result)
