b6f9ec87131e92c947a88e27efd95422b026da99
huang06 <tom4996131207@gmail.com>
2024-02-22 00:51:10 +0800

    feat: utilize batch inference

--- app/main.py
+++ app/main.py
@@ -38,11 +38,8 @@ app = FastAPI()
 async def translation(request_model: RequestModel) -> dict:
     payload = request_model.payload
     tokenizer.src_lang = payload.fromLang
-    result = []
-    for record in payload.records:
-        model_inputs = tokenizer(record.text, return_tensors="pt")
-        generated_tokens = model.generate(**model_inputs, forced_bos_token_id=tokenizer.get_lang_id(payload.toLang))
-        resp = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
-        log.warning("%s, %s", record.text, resp)
-        result.append({"id": record.id, "text": resp})
-    return {"result": result}
+    batch = [record.text for record in payload.records]
+    model_inputs = tokenizer(batch, return_tensors="pt")
+    generated_tokens = model.generate(**model_inputs, forced_bos_token_id=tokenizer.get_lang_id(payload.toLang))
+    outputs = tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)
+    return {"result": [{"id": record.id, "text": output} for record, output in zip(payload.records, outputs)]}
